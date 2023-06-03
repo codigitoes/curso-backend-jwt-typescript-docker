613708b5d2784ffd6aa5c373be96eb334b7713b7
Marco Antonio <mrubio@karecovering.com>
2023-06-03 16:19:45 +0200

    - push sin importancia, permisos antes de modificar .git/config

--- docker/bin/testapi.sh
+++ docker/bin/testapi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it `docker ps | grep curso-typescript.api | head -n1 | awk '{print $1;}'` npm run wtest
+docker exec -it `docker ps | grep curso-backend-jwt-typescript.api | head -n1 | awk '{print $1;}'` npm run wtest
